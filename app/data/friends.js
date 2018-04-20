2818dbaf08041b3e55b4181dd2c481c472538e68
Siyan Zhou <siyanzhou@lawn-128-61-17-224.lawn.gatech.edu>
2018-04-20 00:16:16 -0400

    bug fixed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,20 @@
-var friendData = [];
+var friendData = [
+    {
+        "name":"Ahmed",
+        "photo":"https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+];
 
 module.exports = friendData;
